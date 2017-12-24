9f3299f100b11ee90c59e88ac9eb182706310e1f
Noah Singer <singerng@gmail.com>
2017-12-23 23:13:16 -0500

    sections render properly finally

--- home/views.py
+++ home/views.py
@@ -13,7 +13,7 @@ from core.models import Story, Image, Video, Audio, Section, User
 
 def load_context(request):
     return {
-        "section_roots": Section.objects.all(),
+        "section_roots": Section.objects.filter(parent=None),
         "stories": Story.objects.all()
     }
 
