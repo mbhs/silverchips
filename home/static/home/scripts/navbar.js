87782bd1d5842c9ba39dea94941e0396240021c4
Mark Jung <markojungo@gmail.com>
2018-03-29 14:58:10 -0400

    change navbar animation to more css oriented

--- home/static/home/scripts/navbar.js
+++ home/static/home/scripts/navbar.js
@@ -1,32 +1,10 @@
 $(document).ready(function() {
-    var onResize, onScroll, window_width;
-    var logo = $(".logo").width(),
-        lead = $(".navbar-wrapper-header .lead").css(["height", "padding", "margin"]),
-        navlink = $(".navbar .nav-link").css(["padding"]);
-    onResize = function() {
-        window_width = $(window).width();
-        $(".logo").width(window_width * 0.5);
-        logo = $(".logo").width();
-    };
-    onScroll = _.debounce(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll > 85 && window_width > 992) {
-            $(".logo").animate({
-                "width": logo * 0.75
-            }, 250, "easeOutExpo");
-            $(".navbar .nav-link").animate({
-                "padding": "4px 8px"
-            }, 250, "easeOutExpo");
-        } else if (scroll < 10) {
-            $(".logo").animate({
-                "width": logo
-            }, 250, "easeOutExpo");
-            $(".navbar .nav-link").animate({
-                "padding": navlink["padding"]
-            }, 250, "easeOutExpo");
+    $("body").css("marginTop", $(".navbar-wrapper").height() + 25);
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > 100) {
+            $(".navbar-wrapper").removeClass("not-scrolled").addClass("scrolled");
+        } else {
+            $(".navbar-wrapper").removeClass("scrolled").addClass("not-scrolled");
         }
-    }, 100);
-    onResize(); onScroll();
-    $(window).on("resize", onResize);
-    $(window).on("scroll", onScroll);
+    });
 });
