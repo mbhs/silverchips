202ea403bc29c27f41743d5ce98514caf332730e
Noah Singer <singerng@gmail.com>
2018-05-01 21:17:10 -0400

    ignored static folder
    Former-commit-id: 131693b1e4faf9b42ab5c9702c7a732e01e19e17

--- static/autocomplete_light/jquery.init.js
+++
@@ -1,9 +0,0 @@
-var yl = yl || {};
-
-if (yl.jQuery === undefined) {
-    /* If the user has included another copy of jQuery use that, even in the admin */
-    if (typeof $ !== 'undefined')
-        yl.jQuery = $;
-    else if ((typeof django !== 'undefined') && (typeof django.jQuery !== 'undefined'))
-        yl.jQuery = django.jQuery;
-}
--- static/js/home.js
+++
@@ -1,9 +0,0 @@
-var datetime; (datetime = function() {
-    var now = new Date();
-    var day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][now.getDay()];
-    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][now.getMonth()];
-    var string = day + ", " + month + " " + now.getDate() + ", " + (now.getYear() + 1900);
-    $("#datetime").html(string);
-})();
-
-setInterval(function() { datetime(); }, 1000);
