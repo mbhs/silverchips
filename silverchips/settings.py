9f282b491ffbb36c9b6e717ae8c35d8ed13fa659
Nonameentered <matthewshu88@gmail.com>
2020-04-03 19:08:52 -0400

    change page size to 10

--- silverchips/settings.py
+++ silverchips/settings.py
@@ -143,7 +143,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 25,
+    'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
